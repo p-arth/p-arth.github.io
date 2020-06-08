7cd7a39d6e2a4e15ae7ac1d5cadd104c21f1cf6d
Pedro Arthur <pedro.art.vel@gmail.com>
2020-06-08 17:14:21 -0300

    Finished skills sections, started work section

--- assets/main.js
+++ assets/main.js
@@ -102,3 +102,11 @@ jQuery(document).ready(function($) {
 
 
 TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
+
+
+// Tootip
+
+tippy('[data-tippy-content]', {
+  animation: 'scale',
+  theme: 'p-gradient',
+});
